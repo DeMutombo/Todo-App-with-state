b5aac80c29ac9f3aa4c23c3b3443f193f525f00a
Mutombo <mgtshomba@gmail.com>
2020-11-29 06:17:13 +0200

    ttaking data from the child componet and displaying it, and passing functions via props

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -6,18 +6,19 @@ class TodoList extends Component {
     constructor(props){
         super(props)
         this.state = {
-            todos:[ {todo:"Go running", id:1}]
+            todos:[]
         }
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleSubmit(todo){
+        let newTodo ={...todo, id:1, complete: false }
         this.setState(st=>({
-            todos: [...st.todos, todo]
+            todos: [...st.todos, newTodo]
         }))
     }
     render(){
-        const todo = this.state.todos.map(todo => (<TodoItem todo={todo.todo} id={todo.id}/>))
+        const todo = this.state.todos.map(todo => (<TodoItem todo={todo.todoItem} id={todo.id}/>))
         return(
             <div>
                 <h2>Out Todo List</h2>
