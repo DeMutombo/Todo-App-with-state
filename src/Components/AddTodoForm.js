209f7b2db49335a2c8f581be490fead0fbab6a09
Mutombo <mgtshomba@gmail.com>
2020-11-29 06:15:17 +0200

    Passing the formdata to the parent state and clearing the form on submit

--- src/Components/AddTodoForm.js
+++ src/Components/AddTodoForm.js
@@ -4,24 +4,24 @@ class AddTodoForm extends Component {
     constructor(props){
         super(props)
         this.state ={
-
+            todoItem: ''
         }
         this.handleChange = this.handleChange.bind(this);
         this.submitTodo = this.submitTodo.bind(this)
     }
 
     handleChange(event){
-        this.setState({todo: event.target.value})
+        this.setState({todoItem: event.target.value})
     }
     submitTodo(event){
         event.preventDefault();
-        this.props.addTodo(this.state.todo)
-        this.setState({todo: ''})
+        this.props.addTodo(this.state)
+        this.setState({todoItem: ''})
     }
     render(){
         return(
             <form onSubmit={this.submitTodo}>
-                <input type="text" value={this.state.value} placeholder="Add a todo" onChange={this.handleChange}/>
+                <input type="text" value={this.state.todoItem} placeholder="Add a todo" onChange={this.handleChange}/>
                 <button type="submit"> Add Todo </button>
             </form>
         )
